7a7b26dfdd58ad4e5705dfc3d8044102347653d3
nyarla <nyarladev@gmail.com>
2023-03-16 20:53:52 +0300

    Setup

---
+++ Assets/Scripts/Extentions/Transformable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Extentions
+{
+    public class Transformable : MonoBehaviour
+    {
+        private Transform _transform;
+        
+        public Transform Transform => _transform ??= gameObject.transform;
+
+        [Obsolete]
+        public new Transform transform => Transform;
+    }
+}
